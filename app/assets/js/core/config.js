a60e6333c3ae0e973fc5c813aeb7674877b53819
yasseralkhudairy77 <260947689+yasseralkhudairy77@users.noreply.github.com>
2026-04-08 15:30:38 +0700

    Update Google Apps Script endpoint

--- app/assets/js/core/config.js
+++ app/assets/js/core/config.js
@@ -4,7 +4,7 @@ export const CONFIG = {
   resultKeyPrefix: "HRRS_RESULT_",
   api: {
     enabled: true,
-    endpoint: "https://script.google.com/macros/s/AKfycbyRM9lx8C9DE9bZ9RqqsH_RwYm8Hp1eYyzQAroJRF6JqiJ7V3Hu5yodbIFmRViQdDQ7Dg/exec",
+    endpoint: "https://script.google.com/macros/s/AKfycbzl5q2FyS07nV-yWqL9JBoBpvrhTS595vaiAwLljZHaeS4GoBs9PCByKI7jY4Wf5KD46A/exec",
     timeoutMs: 15000
   }
 };
